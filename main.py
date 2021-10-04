3d5a70e7f720794740f6b5a60386e81455752cd4
Mateo Sam <sammateo4@gmail.com>
2021-10-04 15:55:40 -0400

    Gender prediction API added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from discord.ext import commands
 import discord
 from dotenv import load_dotenv
+import pip._vendor.requests as requests
 
 load_dotenv()  # take environment variables from .env.
 
@@ -72,12 +73,27 @@ async def info(ctx):
         f'Commands: \n'
         f'!solve exp - exp is a numerical expression to be solved using python operators (+,-,/,*,**)\n'
         f'!ping      - returns pong\n'
+        f'!gender name - name is the name of a person. This command will try to predict the gender based on the name entered\n'
     )
 
 
 @bot.command()
 async def ping(ctx):
     await ctx.send("pong")
+
+
+@bot.command()
+async def gender(ctx, arg):
+    url = "https://api.genderize.io/?name="+arg
+    response = requests.get(url)
+    response = response.json()
+    await ctx.send(
+
+        f'Name: {response["name"]}\n'
+        f'Gender: {response["gender"]}\n'
+        f'Probability: {response["probability"]}\n'
+    )
+
 
 # client.run(os.getenv("TOKEN"))
 bot.run(os.getenv("TOKEN"))
